90a500ea426a8d31f5e88038f8a42a6096dda3e1
Aran Elkington <forthold@MAAS-NEOTECH001.localdomain>
2012-02-23 19:55:39 +1000

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject noirtest "0.1.0-SNAPSHOT"
+            :description "Noir Test Forthol"
+            :dependencies [[org.clojure/clojure "1.3.0"]
+                           [noir "1.2.1"]]
+            :main noirtest.server)
+
