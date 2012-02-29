5e8682adc9f4e5fd87e101567d51b8009511a4c9
forthold <aran@forthold.com>
2012-03-01 00:27:14 +1000

    Index and adding mention nodes working

--- src/forthold/graphtag/server.clj
+++ src/forthold/graphtag/server.clj
@@ -27,62 +27,23 @@
            [slingshot ExceptionInfo]))
 
 ;(server/load-views "src/forthold/graphtag/views/")
+
 ;;debugging parts of expressions
 (defmacro dbg[x] `(let [x# ~x] (println "dbg:" '~x "=" x#) x#))
 
+;start quartz
 (sched/initialize)
 (sched/start)
 
-;(def endpoint (neorest/connect! "http://localhost:7474/db/data/"))
-
-;(neorest/connect! "http://localhost:7474/db/data/")
-
+;; Define creds for twitter
 (def ^:dynamic *creds* (make-oauth-creds "GkqZgjg4QikY4lBt1G1A9A"
                          "TOCOK6S2w3ytKIZ8gi3iZ8RFewU5tA7kYebhiToxE2U" 
                          "491183182-M8HQywLDXLVVYuMPBNg03ZW58Ox6ysgHH9Bkc8QA"
                          "bGnRSufusyVki3gSJbaXvhZK1MKqewVw8E00aSfT48"))
 
-; simply retrieves the user, authenticating with the above credentials
-; note that anything in the :params map gets the -'s converted to _'s
-;(show-followers :oauth-creds *creds* :params {:screen-name "graphtag"})
-
-; shows the users friends, without using authentication
-;(show-friends :params {:screen-name "graphtag"})
-
-;(def response-error-throw (println "Error"))
-;(def exception-rethrow (println "Exception"))
-
-;; Make a OAuth consumer
-;(def oauth-consumer (oauth/make-consumer "GkqZgjg4QikY4lBt1G1A9A"
-;                                         "TOCOK6S2w3ytKIZ8gi3iZ8RFewU5tA7kYebhiToxE2U" 
-;                                         "https://api.twitter.com/oauth/request_token"
-;                                         "https://api.twitter.com/oauth/access_token"
-;                                         "https://api.twitter.com/oauth/authorize"
-;                                         :hmac-sha1))
-;
-;(def oauth-access-token )
-;     ;; Look up an access token you've stored away after the user
-;     ;; authorized a request token and you traded it in for an
-;     ;; access token.  See clj-oauth (http://github.com/mattrepl/clj-oauth) for an example.)
-;(def oauth-access-token-secret)
-;     ;; The secret included with the access token)
-;
-;;; Post to twitter
-;(twitter/with-oauth oauth-consumer 
-;                    oauth-access-token
-;                    oauth-access-token-secret
-;                    (twitter/update-status "posting from #clojure with #oauth"))
-;
-; 
-;; Find out who follows dons
-;    (dbg (show-friends :oauth-creds *creds* :callbacks (SyncSingleCallback. response-return-body response-throw-error exception-rethrow) :params {:screen-name "graphtag"}))
-
+;; Setup Neo4j connection
 (neorest/connect! "http://localhost:7474/db/data/")
 
-
-;;
-;; Connections/Discovery
-;;
 
 (defn test-connection-and-discovery-using-connect-with-string-uri []
   (let [endpoint (neorest/connect "http://localhost:7474/db/data/")]
@@ -90,91 +51,75 @@
     (println (:version endpoint))
     ))
 
-;(neorest/connect "http://localhost:7474/db/data/")
 
-(defn mentionHandler [mentions] 
-   (let [men_text (replace-first (:text (nth mentions 0)) #"@graphtag" "")]
-      (println "adsfsdfasdfads" men_text) 
-     )
-  )
+;; Handle a map of mentions keyed on metion id
+;;; Will put id in neo4j index first checking if its there
+;(defn mentionsMapHandler [map_of_mentions] 
+;  ;work out what mentions havent been processed
+;  (let [mention_ids (keys map_of_mentions)]
+;      (println "mention ids =" mention_ids) 
+;     )
+;  )
 
-; Handle a map of mentions keyed on metion id
-;; Will put id in neo4j index first checking if its there
-(defn mentionsMapHandler [map_of_mentions] 
-  ;work out what mentions havent been processed
-  (let [mention_ids (keys map_of_mentions)]
-      (println "mention ids =" mention_ids) 
-     )
-  )
+;(defn test-if-in-index [index mentionid] 
+;    (let [ids (set (map :id (nodes/find index :mentionid mentionid)))]
+;         (dbg ids)
+;        (if (empty? ids)
+;            (nodes/add-to-index mentionid index "mentionid" url1)
+;      )    
+;  )
+(defn get-mention-text [mention] 
+   (replace-first (:text  mention) #"@graphtag" ""))
 
-;(defn GET
-;  [^String uri & { :as options }]
-;  (io!
-;   (http/get uri (merge options { :accept :json }))))
-(defn test-if-in-index [index mentionid] 
-    (let [ids (set (map :id (nodes/find index :mentionid mentionid)))]
-         (dbg ids))    
+(defn get-username [mention] 
+  (let [user (get mention :user) ]
+    (dbg user) 
+    (:screen_name user)))
+;   (let [user (select-keys mention :user)]
+;     (dbg user)
+;     (:screen_name user)))
+;
+(defn new-mention [mention]
+  (println "In new:")  
+  (println "In new mention" (get-mention-text mention)  (get-username mention))
+    ;;; Create new node for mention
+    (let [text (get-mention-text mention) 
+          username (get-username mention)
+          data { :text text :id (:id mention) :username username}
+          node1 (nodes/create data)]
+        (dbg node1)
+        ;;; Add node to index keyed by mentionid  
+        (nodes/add-to-index (:id node1) "node-index-mention-id" "mentionid" (:id mention))
+        ;;; create relationship to user root node
+      )
   )
 ;
-(defn mentionsHandler22 [value] 
-  ;(let [ urlNeo (GET "http://a9d1efcc4.hosted.neo4j.org:7057/db/data/" )]
-    ;(println urlNeo ))
-  ;(let [endpoint (neorest/connect! "http://a9d1efcc4.hosted.neo4j.org:7057/db/data/") ]
-   (println "***** In Mentions Handler: mention = " value)
-   (println "asdfsaf9")
+(defn mention-handler [mention] 
+   (println "***** In Mentions Handler: mention = " mention)
 
-   (test-if-in-index "node-index-mention-id" (:id value))
+   (let [index_name "node-index-mention-id"
+         ids (set (map :id (nodes/find index_name :mentionid (:id mention))))]
+      (dbg ids)
+      (if (empty? ids)
+          (new-mention mention))    
+;   (test-if-in-index "node-index-mention-id" (:id value))
 ;   (let [created-node (nodes/create)]
 ;      (println "node**"  (:id created-node) ))
 ;    ;    (println "asd33" (:version  endpoint))
 ;    ;    (dbg endpoint))
-     )
+     ))
 
 (defrecord JobA []
   org.quartz.Job
   (execute [this ctx]
     (println "**** Job Executing")
-   ;get followers
-    ;(println (show-followers :oauth-creds *creds* :params {:screen-name "graphtag"}))
-;    (println (show-friends :oauth-creds *creds* :callbacks (SyncSingleCallback. response-return-body response-throw-error exception-rethrow) :params {:screen-name "graphtag"}))
-        
-;    (println (home-timeline :oauth-creds *creds* :params {:trim_user "false"} ))
-;     (dbg  (mentions :oauth-creds *creds* :callbacks (SyncSingleCallback. response-return-body response-throw-error exception-rethrow) :params {:trim_user "false"} ))
-       (let [ result (mentions :oauth-creds *creds* :callbacks (SyncSingleCallback. response-return-body response-throw-error exception-rethrow) :params {:trim_user "false"} )] 
-         ;convert vector to map of maps keyed on mention id
-;         (println "*******************wwqqw*" result )
-         ;(println "********************"(:text (nth result 0)))
-         ;(mentionsMapHandler(#(zipmap (map :id %) %) result))
-         ;(let [idv (map vector (iterate inc 0) result)]
-         ;  (dbg idv)  
-         ;(doseq [[value] result] (mentionsHandler22 value))
-         (doseq [value (seq result)] (mentionsHandler22 value))
-         ;(println "are we gettigng here")
-         ;(doseq [value (seq result)] (println "1111111"  value))
-         ;(println "********************2"(nth result 0))
-         )
- 
-                                                                                                                                                                                          ;(get result :text)) )
-     ;(dbg  (mentions :oauth-creds *creds* :params {:trim_user "false"} ))
-;    (println (show-user :oauth-creds *creds* :params {:screen-name "graphtag"} ))
-    ;see if they are new if so send DM
-    ;get mentions
-    ;; if they have code then process and put in neo4j
-    ;; 
-    ))
-
-;(def testQuartz1
-;  (let [job     (j/build
-;                 (j/of-type forthold.graphtag.server.JobA)
-;                 (j/with-identity "job1" "tests"))
-;        trigger  (t/build
-;                  (t/start-now)
-;                  (t/with-schedule (s/schedule
-;                                    (s/with-repeat-count 10)
-;                                    (s/with-interval-in-milliseconds 200))))]
-;    (sched/schedule job trigger)
-;    ))
-
+    ;; Get mentions and map mention-handler over them
+    (let [ result (mentions :oauth-creds *creds* 
+                            :callbacks (SyncSingleCallback. response-return-body response-throw-error exception-rethrow) 
+                            :params {:trim_user "false"} )] 
+      ;(dbg result)   
+      ;(dbg (seq result))
+      (doseq [mention (seq result)] (mention-handler mention)))))
 
 (defn -main [& m]
 ;  (let [mode (keyword (or (first m) :dev))
@@ -186,9 +131,14 @@
   ;;Neo4j config
   (test-connection-and-discovery-using-connect-with-string-uri)
   
-  (let [name "node-index-mention-id"]
-    (nodes/create-index name))
+  ; If index does not exist create it
+  (let [name "node-index-mention-id"
+        list (nodes/all-indexes) ]
+    (if (not (some (fn [i]
+                (= name (:name i))) list))
+    (nodes/create-index name)))
 
+  ;; Schedule Quartz job and trigger
   (let [job     (j/build
                  (j/of-type forthold.graphtag.server.JobA)
                  (j/with-identity "job1" "tests"))
@@ -199,4 +149,3 @@
                                     (s/with-interval-in-milliseconds 2000))))]
     (sched/schedule job trigger))
   )
-  ;(testQuartz1 ))
